b801c0d6659c918fca8d26237ee9c4626d3bc0df
ALEXIS <ALEXIS@PC-ALEXIS>
2020-02-12 17:04:20 -0500

    Proyecto

---
+++ CalculoDeError/src/calculodeerror/CalculoDeError.java
@@ -0,0 +1,17 @@
+
+package calculodeerror;
+
+/**
+ *
+ * @author ALEXIS
+ */
+public class CalculoDeError {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
